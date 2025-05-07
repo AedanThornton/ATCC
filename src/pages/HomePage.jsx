4516daed5b345e21eed4c03f3e965dac58d628f1
AedanThornton <ajt1111@outlook.com>
2025-05-07 16:11:42 -0500

    Unofficial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import "../styles/homepage.css"
 function HomePage() {
   return (
     <div className='home-page'>
-      <h1>Welcome to the <b>Aeon Trespass Card Catalog</b></h1>
+      <h1><span>Welcome to the <i>Unofficial</i></span> <span><b>Aeon Trespass Card Catalog</b></span></h1>
       <nav className='main-nav'>
         <ul>
           <li><Link to="/catalog">Card Catalog</Link></li>
