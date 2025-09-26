6411039c0739c987f7b470caac278f5eabc7b1b6
Aedan Thornton <8935224+AedanThornton@users.noreply.github.com>
2025-09-26 15:23:09 -0500

    Fix: (Maybe Temporary) Tech cards now always are double-sided

--- src/components/CardMenu.jsx
+++ src/components/CardMenu.jsx
@@ -6,7 +6,7 @@ const CardMenu = ({ card, flipFunc, secretFunc, setDisplay }) => {
     <div className="card-menu">
       {/*<Link to={`/card/${card.cardIDs[0]}`}><button>{utils.getIcon("Inspect", undefined, undefined, "1.5em")}</button></Link>*/}
       <button onClick={setDisplay}>{utils.getIcon("Inspect", undefined, undefined, "1.5em")}</button>
-      {card.name2 && (<button onClick={flipFunc}>{utils.getIcon("Flip", undefined, undefined, "1.5em")}</button>)}
+      {(card.name2 || card.cardType === "Technology") && (<button onClick={flipFunc}>{utils.getIcon("Flip", undefined, undefined, "1.5em")}</button>)}
       {(card.foundIn?.includes("Secret Deck") || card.foundIn?.includes("Envelope")) && (<button onClick={secretFunc}>{utils.getIcon("Reveal", undefined, undefined, "1.5em")}</button>)}
     </div>
   )
