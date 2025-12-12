f94fa5c0b7ca08bb6ca93b215f52ef7169da6536
AedanThornton <ajt1111@outlook.com>
2025-12-12 11:19:48 -0600

    New: Pretty Power Dice rendering

--- src/components/WeaponRenderer.jsx
+++ src/components/WeaponRenderer.jsx
@@ -2,6 +2,44 @@ import utils from "./utils";
 import { getGateColor } from "../lib/colors";
 import "/src/styles/weaponStats.css"
 
+const PowerStack = ({ diceArray }) => {
+  const boxHeight = 20 + 5*Math.floor(diceArray.length/2)
+
+  return (
+    <span className="power-dice-group"
+      style={{
+        display: diceArray.length > 1 ? "inline-flex" : "inline",
+        height: `${boxHeight}px`
+      }}
+    >
+      {diceArray.map((src, i, array) => {
+        const depth = i;
+        const dir = i % 2 === 0 ? -1 : 1; // left/right alternating
+
+        const scale = 1 - (depth * 0.05);  // shrink each layer
+        let x = dir * (9 - depth);       // zig-zag offset        
+        const y = -8 * Math.floor(depth/2) * scale - 2*depth + 6*Math.floor((array.length - 1)/2);    // push upward in pairs
+
+        if (src === "Power" || array.length < 2) return utils.getIcon(src, "Power", i+src, `1.5em`)
+        if (i === array.length - 1 && dir === -1) x = 0
+
+        return (
+          <div
+            key={i}
+            className="power-die"
+            style={{
+              zIndex: 100 - depth,
+              transform: `translate(${x}px, ${y}px) scale(${-dir}, 1)`
+            }}
+          >
+            {utils.getIcon(src, "Power", i+src, `${(1 + (0.1 * (3 - Math.floor(depth/2)))) * scale}em`)}
+          </div>
+        );
+      })}
+    </span>
+  );
+}
+
 const WeaponRenderer = ({ statsArray }) => (
   <div className="weapon-stats-bar">
     {statsArray.attackDice && (
@@ -26,9 +64,7 @@ const WeaponRenderer = ({ statsArray }) => (
         )}
         <div className="weapon-stats">
           {p.plus ? '+ ' : ''}
-          {p.type.map((die, subindex) => (
-            utils.getIcon(die, "Power", index + subindex, "1.5em")
-          ))}
+          {<PowerStack diceArray={p.type}/>}
         </div>
       </div>
     ))}
