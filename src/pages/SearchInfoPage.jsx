95ae3b6fba311c1268183e3f4fd7113fe7447f34
AedanThornton <ajt1111@outlook.com>
2026-01-16 11:46:36 -0600

    New: Added "usedfor" documentation to searchinfopage

--- src/pages/SearchInfoPage.jsx
+++ src/pages/SearchInfoPage.jsx
@@ -16,22 +16,27 @@ function SearchInfoPage() {
           <li>
             <b>'any:'</b>
             <p className="search-info__element-description">The 'any:' tag is the most versatile tag, searching for the following parameter within <i>any</i> field of the card (which may result in showing cards without a displayed version of that search term).</p>
-            <p className="search-info__element-example">'any:shark'</p>
+            <p className="search-info__element-example">'any: shark'</p>
           </li>
           <li>
             <b>'id:'</b>
             <p className="search-info__element-description">The 'id:' tag can be used to search for any card ID that contains the following parameter (if an entire ID is specified, that will be the only card displayed).</p>
-            <p className="search-info__element-example">'id:AA0003'</p>
+            <p className="search-info__element-example">'id: AA0003'</p>
           </li>
           <li>
             <b>'traits:'</b>
             <p className="search-info__element-description">The 'traits:' tag can be used to search for the following parameter within the Traits of cards.</p>
-            <p className="search-info__element-example">'traits:wood', 'traits:voyage'</p>
+            <p className="search-info__element-example">'traits: wood', 'traits: voyage'</p>
           </li>
           <li>
             <b>'slot:'</b>
             <p className="search-info__element-description">This tag can be used to narrow the search to only look for Gear cards that have the specified Slot type.</p>
-            <p className="search-info__element-example">"slot:3 hands", "slot:2 1 hands", "slot:support"</p>
+            <p className="search-info__element-example">"slot: 3 hands", "slot: 2 1 hands", "slot: support"</p>
+          </li>
+          <li>
+            <b>'usedfor:'</b>
+            <p className="search-info__element-description">This tag can be used to narrow the search to only look for cards associated with a Primordial whose name contains all or part of the following parameter.</p>
+            <p className="search-info__element-example">"usedfor: pursuer"</p>
           </li>
           <li>
             <b>No tag</b>
@@ -45,12 +50,12 @@ function SearchInfoPage() {
           <li>
             <b>AND (',')</b>
             <p className="search-info__element-description">Search parameters can be combined together by comma-separating them. Cards will only display if they meet all requirements simultaneously.</p>
-            <p className="search-info__element-example">'slot:1 hand, traits:wood' - this will display all cards that have both 1 hand slot and a wood trait.</p>
+            <p className="search-info__element-example">'slot: 1 hand, traits: wood' - this will display all cards that have both 1 hand slot and a wood trait.</p>
           </li>
           <li>
             <b>OR ('||')</b>
             <p className="search-info__element-description">Search parameters can be differentiated by placing a double-pipe between them ('||'). Cards will display if they meet any of the search parameter groups separated this way.</p>
-            <p className="search-info__element-example">'slot:1 hand, traits:wood || slot:2 hands, teeth' - this will display all cards that have both 1 hand slot and a wood trait as well as all two-handed cards with 'teeth' anywhere in the name.</p>
+            <p className="search-info__element-example">'slot: 1 hand, traits: wood || slot: 2 hands, teeth' - this will display all cards that have both 1 hand slot and a wood trait as well as all two-handed cards with 'teeth' anywhere in the name.</p>
           </li>
         </ul>
       </div>
