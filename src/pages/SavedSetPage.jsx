520aa4b976a42dea554944f2ae03ce83d2b688b3
AedanThornton <ajt1111@outlook.com>
2026-05-08 15:19:27 -0500

    New: Saved sets page styling

--- src/pages/SavedSetPage.jsx
+++ src/pages/SavedSetPage.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import CardRenderer from "../components/cards/CardRenderer";
 import { useLocalStorage } from "../context/LocalStorageContext";
 import "../styles/savedsetspage.css"
+import { useModal } from "../context/FocusContext";
 
 function SavedSetPage() {
   const { appState, cardCache } = useLocalStorage();
+  const { openModal } = useModal();
+  
+  const setDisplayHelper = (cardID) => {
+    openModal("focusCard", { id: cardID })
+  }
 
   return (
     <div className="saved-sets-page">
@@ -12,16 +18,21 @@ function SavedSetPage() {
         <div key={i} className="saved-set-row">
           <span className="saved-set-title">{setName}</span>
           <div className="saved-set-cards">
-            {appState.savedSets[setName]?.map((card, j) => (
-              <div key={j} className="saved-sets-card-details">
-                {/* {cardCache.get(card) && <CardRenderer cardData={cardCache.get(card)} variant="backpack" />} */}
-                <span>{cardCache.get(card)?.cardIDs[0]}</span>
-                <span>{cardCache.get(card)?.name}</span>
-                <span>{cardCache.get(card)?.cardType}</span>
-                <span></span>
-                <span></span>
-              </div>
-            ))}
+            <div className="saved-sets-card-details" style={{ borderBottom: "1px solid white", fontSize: "18px", fontWeight: "bold" }}>
+              <span>IDs</span>
+              <span>Name</span>
+              <span>Type</span>
+            </div>
+            <div className="saved-sets-card-list">
+              {appState.savedSets[setName]?.map((card, j) => (
+                <div key={j} className="saved-sets-card-details clickable" onClick={() => setDisplayHelper(cardCache.get(card)?.cardIDs[0])}>
+                  <span>{cardCache.get(card)?.cardIDs.join(", ")}</span>
+                  <span>{cardCache.get(card)?.name}</span>
+                  <span>{cardCache.get(card)?.cardType}</span>
+                  
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       ))}
