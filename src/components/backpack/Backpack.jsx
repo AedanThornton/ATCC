9b1ecaa43a73326b9b733991804b55bbf3211f87
Aedan Thornton <8935224+AedanThornton@users.noreply.github.com>
2026-06-16 09:03:11 -0500

    Fix: Close backpack button overlapping

--- src/components/backpack/Backpack.jsx
+++ src/components/backpack/Backpack.jsx
@@ -7,12 +7,14 @@ import SavedSets from "./SavedSets";
 import DeckPage from "../../pages/DeckPage";
 import Compare from "../compare/Compare";
 import HamburgerButton from "../utils/HamburgerButton";
+import getIcon from "../utils/iconUtils";
 
 const Backpack = ({ isDragging, icon }) => {
-  const [openBackpack, setOpenBackpack] = useState(false)
-  const [showBackpack, setShowBackpack] = useState(false)
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  const [activeView, setActiveView] = useState("Backpack")
+  const [openBackpack, setOpenBackpack] = useState(false);
+  const [showBackpack, setShowBackpack] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [pageOverHover, setPageOverHover] = useState(false)
+  const [activeView, setActiveView] = useState("Backpack");
   const { ref, isDropTarget } = useDroppable({ id: "backpack" });
 
   const views = {
@@ -36,10 +38,19 @@ const Backpack = ({ isDragging, icon }) => {
   return <div ref={ref} className={`page-over ${isDropTarget ? "is-drop-target" : ""} ${(isDragging && !openBackpack) ? "open" : ""} ${showBackpack ? "backpack-show" : ""} ${openBackpack ? "backpack-open" : ""}`}>
     <BackpackTabSelector showBackpack={showBackpack} views={Object.keys(views)} activeView={activeView} setActiveView={setActiveView} />
     <div className={`page-over-arrow ${openBackpack ? "backpack-open" : ""}`}>
-      <div onClick={() => handleOpen()}>{icon}</div>
+      <div
+        className="page-over-arrow-button"
+        onClick={() => handleOpen()}
+        onMouseEnter={() => setPageOverHover(true)}
+        onMouseLeave={() => setPageOverHover(false)}
+      >
+        {(pageOverHover && openBackpack)
+          ? "✖"
+          : getIcon({name: "Backpack", invert: true})
+        }
+      </div>
     </div>
     {showBackpack && <div className="page-over__main">
-      {/* REPLACE BELOW WITH HAMBURGER BUTTON */}
       <div className="page-over__setslist-sidebar__button"><HamburgerButton clickFunc={() => setSidebarOpen(!sidebarOpen)}/></div>
       {sidebarOpen && <div className="page-over__setslist-sidebar">
         <SavedSets />
