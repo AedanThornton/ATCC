8722bc6a82667983e6aac821121a2e0d076ade05
Aedan Thornton <8935224+AedanThornton@users.noreply.github.com>
2026-07-09 09:38:23 -0500

    New: Added Icons to Deck tab buttons

--- src/components/deck/DeckSelector.jsx
+++ src/components/deck/DeckSelector.jsx
@@ -1,3 +1,22 @@
+import getIcon from "../utils/iconUtils"
+
+const getPoolIcon = (poolName) => {
+  switch (poolName) {
+    case "deck":
+      return <span>{getIcon({name: "Deck", invert: true})} </span>
+      break
+    case "discard":
+      return <span>{getIcon({name: "Discard", invert: true})} </span>
+      break
+    case "removed":
+      return <span>{getIcon({name: "Trash", invert: true})} </span>
+      break
+    default:
+      return ""
+      break
+  }
+}
+
 const DeckSelector = ({ deckState }) => {
   return (
     <div className="deck-page__card-pool-selector">
@@ -7,7 +26,7 @@ const DeckSelector = ({ deckState }) => {
           onClick={() => deckState.setActiveCardPool(poolName)}
           disabled={deckState.activeCardPool === poolName}
         >
-          {poolName.charAt(0).toUpperCase() + poolName.slice(1)}
+          {getPoolIcon(poolName)}{poolName.charAt(0).toUpperCase() + poolName.slice(1)}
         </button>
       ))}
     </div>
