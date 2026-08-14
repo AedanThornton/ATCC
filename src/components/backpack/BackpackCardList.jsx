cf67581ab49d608a7a7bb4aa149d4b22ec58795c
AedanThornton <ajt1111@outlook.com>
2026-08-14 11:49:26 -0500

    New: Backpack is droppable

--- src/components/backpack/BackpackCardList.jsx
+++ src/components/backpack/BackpackCardList.jsx
@@ -4,11 +4,20 @@ import CardRenderer from "../cards/CardRenderer";
 import { useState } from "react";
 import { useBackpackContext } from "../../context/BackpackContext";
 import RectractableMenu from "../RetractableMenu";
+import { useDroppable } from "@dnd-kit/react";
 
 const BackpackCardList = ({}) => {
-  const { appState, cardCache } = useLocalStorage();
+  const { appState, cardCache, addToActiveSet } = useLocalStorage();
   const { backpackIsActive } = useBackpackContext();
   const [buttonError, setButtonError] = useState(null)
+  const {ref} = useDroppable({id: "backpack", data: {onDrop: handleBackpackDrop} })
+
+  function handleBackpackDrop(id){
+    if (!id) return
+    const idParts = id.split("-")
+    const realID = idParts[idParts.length - 1]
+    addToActiveSet(realID)
+  }
 
   const buttons = [
     {
@@ -20,7 +29,7 @@ const BackpackCardList = ({}) => {
 
   return (
     <>
-      <div className={`backpack-card-list`}>
+      <div className={`backpack-card-list`} ref={ref}>
         {appState.activeSet?.map((card, i) => 
           <div className="drag-backpack-item" key={i}>
             {cardCache.get(card) && <CardRenderer cardData={cardCache.get(card)} variant={backpackIsActive ? "backpack" : "workspace"} />}
