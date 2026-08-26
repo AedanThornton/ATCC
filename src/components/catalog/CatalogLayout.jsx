da794d30d32d948dcaf90c9cd97cfa51e434b4c3
AedanThornton <ajt1111@outlook.com>
2026-08-26 11:19:01 -0500

    Fix: Hide menu buttons while SavedSets panel is open

--- src/components/catalog/CatalogLayout.jsx
+++ src/components/catalog/CatalogLayout.jsx
@@ -4,19 +4,22 @@ import SearchBar from './SearchBar.jsx';
 import ControlBar from './ControlBar.jsx';
 import HamburgerButton from '../utils/HamburgerButton.jsx';
 import FilterSidebar from './FilterSidebar.jsx'
+import { useSavedSetsContext } from '../../context/SavedSetsContext.jsx';
 
 export const CatalogLayoutTopbar = () => {
   const [isFilterSidebarOpen, setIsFilterSidebarOpen] = useState(false);
+  const { savedSetsOpen } = useSavedSetsContext()
+  const isNotMobile = window.matchMedia('(hover: hover)').matches;
 
   return <>
     <ControlBar />
 
     <div className='top-menu'>
-      <div className='filters-menu'>
+      {!(savedSetsOpen && !isNotMobile) && <div className='filters-menu'>
         <HamburgerButton clickFunc={() => setIsFilterSidebarOpen(!isFilterSidebarOpen)} />
 
         {isFilterSidebarOpen && <FilterSidebar />}
-      </div>
+      </div>}
     </div>
   </>
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,10 +4,13 @@ import logo from "../../assets/ATCC.svg"
 import { useLayout } from '../../context/LayoutContext';
 import getIcon from '../utils/iconUtils';
 import { useState } from 'react';
+import { useSavedSetsContext } from '../../context/SavedSetsContext';
 
 function Layout({}) {
   const { layout } = useLayout()
   const [navOpen, setNavOpen] = useState(false)
+  const { savedSetsOpen } = useSavedSetsContext()
+  const isNotMobile = window.matchMedia('(hover: hover)').matches;
 
   return (
     <div className="app-layout">
@@ -15,7 +18,7 @@ function Layout({}) {
         <div className="layout-row">
           <div className='top-menu'>
             <nav className='layout-nav'>
-              <button className='menu-button'
+              {!(savedSetsOpen && !isNotMobile) && <button className='menu-button'
                 onPointerOver={() => setNavOpen(true)}
               >
                 <img src={logo} alt='Menu' width="40" height="40"/>
@@ -30,7 +33,7 @@ function Layout({}) {
                     <Link onClick={() => setNavOpen(false)} to="https://ko-fi.com/artifus" target="_blank" rel="noopener noreferrer"><li>Support Me <img src="https://storage.ko-fi.com/cdn/logomarkLogo.png" alt=' on Ko-fi' width="35" height="30" /></li></Link>
                   </ul>
                 </div>
-              </button>
+              </button>}
             </nav>
           </div>
 
